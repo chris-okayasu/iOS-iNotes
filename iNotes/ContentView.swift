8c6b09c6df61740ffba4757f1f463bfceac3098e
Chris <san.kyu@icloud.com>
2024-11-15 14:10:56 -0600

    feat: added a view to list all notes
    also inyected some values to the #Preview usiong the .init()

--- iNotes/ContentView.swift
+++ iNotes/ContentView.swift
@@ -10,11 +10,19 @@ import SwiftData
 
 struct ContentView: View {
     var viewModel: ViewModel = .init()
+    
     var body: some View {
         NavigationStack {
             List {
                 ForEach(viewModel.notes) { note in
-                    
+                    NavigationLink(value: note){
+                        VStack(alignment: .leading){
+                            Text(note.title)
+                                .foregroundStyle(.primary)
+                            Text(note.getText)
+                                .foregroundStyle(.secondary)
+                        }
+                    }
                 }
             }
         }
@@ -22,5 +30,9 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ContentView(viewModel: .init(notes: [
+        .init(title: "Hello", text: "World", createdAt: .now),
+        .init(title: "SwiftUI", text: "Is Awesome", createdAt: .now),
+        .init(title: "Swift", text: "Is Cool", createdAt: .now)
+    ]))
 }
