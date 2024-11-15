fbbf1dc7d20849dba8402c05bff27e42af4f03b0
Chris <san.kyu@icloud.com>
2024-11-15 15:24:29 -0600

    test: when text from note object is empty or nil

--- iNotesTests/NoteTest.swift
+++ iNotesTests/NoteTest.swift
@@ -20,23 +20,25 @@ final class NoteTest: XCTestCase {
         // When or Act
         let note = Note(title: title, text: text, createdAt: date)
         
-        // Then or Assert
+        // Then or Assert (what I am expecting)
         XCTAssertEqual(note.title, title, "title should be equal to \(title).")
         XCTAssertEqual(note.text, text, "text should be equal to \(text).")
         XCTAssertEqual(note.createdAt, date, "date should be equal to \(date).")
     }
     
-    func testTextIsEmpty() {
+    func testNoteTextIsEmpty() {
+        
+        // Given
         let title = "Hello, World!"
-        let text = ""
         let date = Date()
         
-        let note = Note(title: title, text: text, createdAt: date)
+        // When
+        let note = Note(title: title, text: nil, createdAt: date)
         
-        XCTAssertEqual(note.title, title, "title should be equal to \(title).")
-        XCTAssertEqual(note.text, "", "text should be empty. But it is \(text).")
-        XCTAssertEqual(note.createdAt, date, "date should be equal to \(date).")
+        // Then
+        XCTAssertEqual(note.getText, "", "text should be empty. But it is \(note.getText).")
     }
+    
 
     
     
