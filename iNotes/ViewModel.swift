5b354f1d5cbb4e3b1d42e516b2f29dc8c0644579
Chris <san.kyu@icloud.com>
2024-11-15 12:23:32 -0600

    feat: basic ViewModel added

---
+++ iNotes/ViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModel.swift
+//  iNotes
+//
+//  Created by chris on 2024/11/15.
+//
+
+import Foundation
+
+class ViewModel {
+    var notes: [Note]
+    
+    init(notes: [Note] = []) {
+        self.notes = notes
+    }
+    
+}
